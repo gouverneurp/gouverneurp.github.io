b01480a9b253961a24a843bec136ff3aa7448c37
Philip Gouverneur <philipgouverneur@gmx.de>
2023-12-08 22:40:52 +0100

    Fixed particle canvas for mobile

--- js/js.js
+++ js/js.js
@@ -228,7 +228,7 @@ document.addEventListener("DOMContentLoaded", function () {
     new particlesJS("lead", {
         particles: {
             number: {
-                value: 30,
+                value: 50,
                 density: {
                     enable: !0,
                     value_area: 800

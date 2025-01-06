6945bb7109c3d0d2dfe93644e3a2e3301b37a550
GitHub Action <action@github.com>
2025-01-06 03:48:58 +0000

    Updated citations

--- js/js.js
+++ js/js.js
@@ -69,7 +69,7 @@ function createBarChart() {
         xAxis: [
             {
                 type: 'category',
-                data/*xaxis*/: ['2018', '2019', '2020', '2021', '2022', '2023', '2024', 'Overall'],
+                data/*xaxis*/: ['2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025', 'Overall'],
                 axisTick: {
                     alignWithLabel: true
                 }
@@ -85,7 +85,7 @@ function createBarChart() {
                 name: 'Citations',
                 type: 'bar',
                 barWidth: '60%',
-                data/*yaxis*/: [1, 7, 10, 23, 33, 73, 88, { value: 235, itemStyle: { color: '#a90000' } },],
+                data/*yaxis*/: [1, 7, 10, 23, 33, 73, 88, 1, { value: 236, itemStyle: { color: '#a90000' } },],
                 itemStyle: {
                     borderRadius: 3,
                     borderWidth: 1,

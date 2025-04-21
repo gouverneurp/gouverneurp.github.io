70dbea25278393428a570a165211cc0755f875c0
Philip Gouverneur <philipgouverneur@gmx.de>
2025-04-21 17:49:09 +0200

    Updated citations

--- js/js.js
+++ js/js.js
@@ -69,7 +69,7 @@ function createBarChart() {
         xAxis: [
             {
                 type: 'category',
-                data/*xaxis*/: ['2018', '2019', '2020', '2021', '2022', '2023', '2024', 'Overall'],
+                data/*xaxis*/: ['2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025', 'Overall'],
                 axisTick: {
                     alignWithLabel: true
                 }
@@ -85,7 +85,7 @@ function createBarChart() {
                 name: 'Citations',
                 type: 'bar',
                 barWidth: '60%',
-                data/*yaxis*/: [1, 7, 10, 22, 34, 72, 45, { value: 191, itemStyle: { color: '#a90000' } },],
+                data/*yaxis*/: [1, 7, 9, 22, 38, 79, 81, 33, { value: 270, itemStyle: { color: '#a90000' } },],
                 itemStyle: {
                     borderRadius: 3,
                     borderWidth: 1,

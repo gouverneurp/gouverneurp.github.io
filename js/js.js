067dc281a3bc0b394d43e3296877a04a0e399f31
GitHub Action <action@github.com>
2025-12-08 04:18:05 +0000

    Updated citations

--- js/js.js
+++ js/js.js
@@ -85,7 +85,7 @@ function createBarChart() {
                 name: 'Citations',
                 type: 'bar',
                 barWidth: '60%',
-                data/*yaxis*/: [1, 7, 9, 23, 39, 81, 85, 93, { value: 338, itemStyle: { color: '#a90000' } },],
+                data/*yaxis*/: [1, 7, 9, 23, 39, 81, 85, 97, { value: 342, itemStyle: { color: '#a90000' } },],
                 itemStyle: {
                     borderRadius: 3,
                     borderWidth: 1,
